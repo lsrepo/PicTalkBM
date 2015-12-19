9d2a34a555897e3664911bbacb4be26a1ec8966d
Pak <radishlau@gmail.com>
2015-12-19 02:43:16 +0100

    4 pics working automatically

--- PicTalk.js
+++ PicTalk.js
@@ -7,32 +7,29 @@ if (Meteor.isClient) {
       "Arabic": "بلاستر , لاصق طبي ",
       "English": "Band aid",
       "URL": "http://bildstod.se/image/get/7869",
-      "File": "band_aid.jpg"
+      "File": "images/band_aid.jpg"
     }, {
       "Kategori": "Food",
       "Swedish": "Öl",
       "Arabic": "بيرة",
       "English": "Beer",
       "URL": "http://bildstod.se/image/get/2769",
-      "File": "beer.jpg"
+      "File": "images/beer.jpg"
     }, {
       "Kategori": "Transportation",
       "Swedish": "Cykel",
       "Arabic": "دراجة",
       "English": "Bicycle",
       "URL": "http://bildstod.se/image/get/2702",
-      "File": "bicycle.jpg"
+      "File": "images/bicycle.jpg"
     }, {
       "Kategori": "Core vocabulary",
       "Swedish": "Barn",
       "Arabic": "أولاد",
       "English": "Children",
       "URL": "http://symbolmanager.ecs.soton.ac.uk/images/ARASAAC/copyof-boys_2.png",
-      "File": "children.png"
+      "File": "images/children.png"
     }]
-
-
-
   });
 }
 
